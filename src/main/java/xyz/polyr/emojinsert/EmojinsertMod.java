65a7dbd122dc8b2c95ac1bc350f35fe8e58e283e
Polyr <17569708+Polyr@users.noreply.github.com>
2018-08-16 20:08:55 -0400

    Add more default emoji

--- src/main/java/xyz/polyr/emojinsert/EmojinsertMod.java
+++ src/main/java/xyz/polyr/emojinsert/EmojinsertMod.java
@@ -25,10 +25,12 @@ public class EmojinsertMod {
             config = new Configuration(event.getSuggestedConfigurationFile());
             config.load();
             config.get(EMOJI_MAP_CATEGORY, "<3", "\u2764");
-            config.get(EMOJI_MAP_CATEGORY, ":star:", "\u272e");
-            config.get(EMOJI_MAP_CATEGORY, ":shrug:", "\u00af\\_(\u30c4)_/\u00af");
-            config.get(EMOJI_MAP_CATEGORY, ":tableflip:", "(\u256f\u00b0\u25a1\u00b0\uff09\u256f\ufe35 \u253b\u2501\u253b");
-            config.get(EMOJI_MAP_CATEGORY, "o/", "( \uff9f\u25e1\uff9f)/");
+            config.get(EMOJI_MAP_CATEGORY, ":star:", "\u272E");
+            config.get(EMOJI_MAP_CATEGORY, ":shrug:", "\u00AF\\_(\u30C4)_/\u00AF");
+            config.get(EMOJI_MAP_CATEGORY, ":tableflip:", "(\u256F\u00B0\u25A1\u00B0\uFF09\u256F\uFE35 \u253B\u2501\u253B");
+            config.get(EMOJI_MAP_CATEGORY, ":unflip:", "\u252C\u2500\u252C \u30CE( \u309C-\u309C\u30CE)");
+            config.get(EMOJI_MAP_CATEGORY, "o/", "( \uFF9F\u25E1\uFF9F)/");
+            config.get(EMOJI_MAP_CATEGORY, "\\o", "\\(\uFF9F\u25E1\uFF9F )");
 
             if (config.hasChanged()) {
                 config.save();
